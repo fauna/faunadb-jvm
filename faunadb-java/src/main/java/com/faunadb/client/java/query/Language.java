9c815936d747e6aeca09bde3e077cba39e62fc34
Ed Ceaser <ed@faunadb.com>
2015-06-15 09:42:11 -0700

    Static helpers.

---
+++ faunadb-java/src/main/java/com/faunadb/client/java/query/Language.java
@@ -0,0 +1,208 @@
+package com.faunadb.client.java.query;
+
+import com.faunadb.client.java.query.Value.*;
+import com.faunadb.client.java.types.Ref;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+
+public abstract class Language {
+  public static Value.ObjectV ObjectV() {
+    return ObjectV.empty();
+  }
+
+  public static ObjectV ObjectV(ImmutableMap<String, Value> values) {
+    return ObjectV.create(values);
+  }
+
+  public static ObjectV ObjectV(String k1, Value v1) {
+    return ObjectV.create(k1, v1);
+  }
+
+  public static ObjectV ObjectV(String k1, Value v1, String k2, Value v2) {
+    return ObjectV.create(k1, v1, k2, v2);
+  }
+
+  public static ObjectV ObjectV(String k1, Value v1, String k2, Value v2, String k3, Value v3) {
+    return ObjectV.create(k1, v1, k2, v2, k3, v3);
+  }
+
+  public static ObjectV ObjectV(String k1, Value v1, String k2, Value v2, String k3, Value v3, String k4, Value v4) {
+    return ObjectV.create(k1, v1, k2, v2, k3, v3, k4, v4);
+  }
+
+  public static ObjectV ObjectV(String k1, Value v1, String k2, Value v2, String k3, Value v3, String k4, Value v4, String k5, Value v5) {
+    return ObjectV.create(k1, v1, k2, v2, k3, v3, k4, v4, k5, v5);
+  }
+
+  public static ArrayV ArrayV() {
+    return ArrayV.empty();
+  }
+
+  public static ArrayV ArrayV(ImmutableList<Value> values) {
+    return ArrayV.create(values);
+  }
+
+  public static ArrayV ArrayV(Value v1) {
+    return ArrayV.create(v1);
+  }
+
+  public static ArrayV ArrayV(Value v1, Value v2) {
+    return ArrayV.create(v1, v2);
+  }
+
+  public static ArrayV ArrayV(Value v1, Value v2, Value v3) {
+    return ArrayV.create(v1, v2, v3);
+  }
+
+  public static ArrayV ArrayV(Value v1, Value v2, Value v3, Value v4) {
+    return ArrayV.create(v1, v2, v3, v4);
+  }
+
+  public static ArrayV ArrayV(Value v1, Value v2, Value v3, Value v4, Value v5) {
+    return ArrayV.create(v1, v2, v3, v4, v5);
+  }
+
+  public static ArrayV ArrayV(Value v1, Value v2, Value v3, Value v4, Value v5, Value v6) {
+    return ArrayV.create(v1, v2, v3, v4, v5, v6);
+  }
+
+  public static Ref Ref(String ref) {
+    return Ref.create(ref);
+  }
+
+  public static RefV RefV(Ref ref) {
+    return RefV.create(ref);
+  }
+
+  public static RefV RefV(String value) {
+    return RefV.create(value);
+  }
+
+  public static VarV VarV(String value) {
+    return VarV.create(value);
+  }
+
+  public static StringV StringV(String value) {
+    return StringV.create(value);
+  }
+
+  public static NumberV NumberV(long value) {
+    return NumberV.create(value);
+  }
+
+  public static DoubleV DoubleV(double value) {
+    return DoubleV.create(value);
+  }
+
+  public static BooleanV BooleanV(boolean value) {
+    return BooleanV.create(value);
+  }
+
+  public static Count Count(Set set) {
+    return Count.create(set);
+  }
+
+  public static Create Create(Identifier ref) {
+    return Create.create(ref);
+  }
+
+  public static Create Create(Ref ref) {
+    return Create.create(ref);
+  }
+
+  public static Create Create(Identifier ref, ObjectV params) {
+    return Create.create(ref, params);
+  }
+
+  public static Create Create(Ref ref, ObjectV params) {
+    return Create.create(ref, params);
+  }
+
+  public static Delete Delete(Identifier ref) {
+    return Delete.create(ref);
+  }
+
+  public static Difference Difference(ImmutableList<Set> sets) {
+    return Difference.create(sets);
+  }
+
+  public static Do Do(ImmutableList<Expression> expressions) {
+    return Do.create(expressions);
+  }
+
+  public static Foreach Foreach(Lambda lambda, Expression collection) {
+    return Foreach.create(lambda, collection);
+  }
+
+  public static Get Get(Identifier resource) {
+    return Get.create(resource);
+  }
+
+  public static If If(Expression condition, Expression then, Expression elseExpression) {
+    return If.create(condition, then, elseExpression);
+  }
+
+  public static Intersection Intersection(ImmutableList<Set> sets) {
+    return Intersection.create(sets);
+  }
+
+  public static Join Join(Set source, Lambda target) {
+    return Join.create(source, target);
+  }
+
+  public static Lambda Lambda(String argument, Expression expr) {
+    return Lambda.create(argument, expr);
+  }
+
+  public static Let Let(ImmutableMap<String, Expression> vars, Expression in) {
+    return Let.create(vars, in);
+  }
+
+  public static Map Map(Lambda lambda, Expression collection) {
+    return Map.create(lambda, collection);
+  }
+
+  public static Match Match(Ref term, Ref index) {
+    return Match.create(term, index);
+  }
+
+  public static Match Match(Value term, Ref index) {
+    return Match.create(term, index);
+  }
+
+  public static Paginate Paginate(Identifier resource) {
+    return Paginate.create(resource);
+  }
+
+  public static Quote Quote(Expression expression) {
+    return Quote.create(expression);
+  }
+
+  public static Replace Replace(Identifier ref, ObjectV obj) {
+    return Replace.create(ref, obj);
+  }
+
+  public static Select Select(ImmutableList<Path> path, Value from) {
+    return Select.create(path, from);
+  }
+
+  public static Union Union(ImmutableList<Set> sets) {
+    return Union.create(sets);
+  }
+
+  public static Update Update(Identifier ref, ObjectV params) {
+    return Update.create(ref, params);
+  }
+
+  public static Var Var(String variable) {
+    return Var.create(variable);
+  }
+
+  public static Cursor.Before Before(Ref ref) {
+    return Cursor.Before.create(ref);
+  }
+
+  public static Cursor.After After(Ref ref) {
+    return Cursor.After.create(ref);
+  }
+}
--- faunadb-java/src/test/java/com/faunadb/client/java/ClientSpec.java
+++ faunadb-java/src/test/java/com/faunadb/client/java/ClientSpec.java
@@ -22,7 +22,7 @@ import java.io.IOException;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
-import static com.faunadb.client.java.query.Value.*;
+import static com.faunadb.client.java.query.Language.*;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
@@ -49,20 +49,20 @@ public class ClientSpec {
   @BeforeClass
   public static void beforeAll() throws IOException, ExecutionException, InterruptedException {
     rootClient = FaunaClient.create(Connection.builder().withFaunaRoot(config.get("root_url")).withAuthToken(config.get("root_token")).build());
-    ListenableFuture<ResponseNode> dbCreateF = rootClient.query(Create.create(Ref.create("databases"), ObjectV("name", StringV(testDbName))));
+    ListenableFuture<ResponseNode> dbCreateF = rootClient.query(Create(Ref("databases"), ObjectV("name", StringV(testDbName))));
     ResponseNode dbCreateR = dbCreateF.get();
     Ref dbRef = dbCreateR.asDatabase().ref();
 
-    ListenableFuture<ResponseNode> keyCreateF = rootClient.query(Create.create(Ref.create("keys"), ObjectV("database", RefV(dbRef), "role", StringV("server"))));
+    ListenableFuture<ResponseNode> keyCreateF = rootClient.query(Create(Ref("keys"), ObjectV("database", RefV(dbRef), "role", StringV("server"))));
     ResponseNode keyCreateR = keyCreateF.get();
     Key key = keyCreateR.asKey();
 
     client = FaunaClient.create(Connection.builder().withFaunaRoot(config.get("root_url")).withAuthToken(key.secret()).build());
 
-    ListenableFuture<ResponseNode> classCreateF = client.query(Create.create(Ref.create("classes"), ObjectV("name", StringV("spells"))));
+    ListenableFuture<ResponseNode> classCreateF = client.query(Create(Ref("classes"), ObjectV("name", StringV("spells"))));
     classCreateF.get();
 
-    ListenableFuture<ResponseNode> indexCreateF = client.query(Create.create(Ref.create("indexes"), ObjectV(
+    ListenableFuture<ResponseNode> indexCreateF = client.query(Create(Ref("indexes"), ObjectV(
       "name", StringV("spells_by_test"),
       "source", RefV("classes/spells"),
       "path", StringV("data.queryTest1"),
@@ -70,7 +70,7 @@ public class ClientSpec {
 
     indexCreateF.get();
 
-    ListenableFuture<ResponseNode> setIndexF = client.query(Create.create(Ref.create("indexes"), ObjectV(
+    ListenableFuture<ResponseNode> setIndexF = client.query(Create(Ref("indexes"), ObjectV(
       "name", StringV("spells_instances"),
       "source", RefV("classes/spells"),
       "path", StringV("class"),
@@ -79,7 +79,7 @@ public class ClientSpec {
 
     setIndexF.get();
 
-    ListenableFuture<ResponseNode> uniqueIndexF = client.query(Create.create(Ref.create("indexes"), ObjectV(
+    ListenableFuture<ResponseNode> uniqueIndexF = client.query(Create(Ref("indexes"), ObjectV(
       "name", StringV("spells_by_unique_test"),
       "source", RefV("classes/spells"),
       "path", StringV("data.uniqueTest1"),
@@ -91,7 +91,7 @@ public class ClientSpec {
 
   @Test(expected=NotFoundQueryException.class)
   public void testLookupMissingInstance() throws Throwable {
-    ListenableFuture<ResponseNode> resp = client.query(Get.create(RefV("classes/spells/1234")));
+    ListenableFuture<ResponseNode> resp = client.query(Get(RefV("classes/spells/1234")));
     try {
       resp.get();
     } catch (ExecutionException ex) {
@@ -101,14 +101,14 @@ public class ClientSpec {
 
   @Test
   public void testCreateNewInstance() throws IOException, ExecutionException, InterruptedException {
-    ListenableFuture<ResponseNode> respF = client.query(Create.create(Ref.create("classes/spells"), ObjectV("data", ObjectV("testField", StringV("testValue")))));
+    ListenableFuture<ResponseNode> respF = client.query(Create(Ref("classes/spells"), ObjectV("data", ObjectV("testField", StringV("testValue")))));
     Instance resp = respF.get().asInstance();
 
     assertThat(resp.ref().value(), startsWith("classes/spells/"));
     assertThat(resp.classRef().value(), is("classes/spells"));
     assertThat(resp.data().get("testField").asString(), is("testValue"));
 
-    ListenableFuture<ResponseNode> resp2F = client.query(Create.create(Ref.create("classes/spells"), ObjectV("data", ObjectV("testField", ObjectV("array", ArrayV(NumberV(1), StringV("2"), DoubleV(3.4)), "bool", BooleanV(true), "num", NumberV(1234), "string", StringV("sup"), "float", DoubleV(1.234))))));
+    ListenableFuture<ResponseNode> resp2F = client.query(Create(Ref("classes/spells"), ObjectV("data", ObjectV("testField", ObjectV("array", ArrayV(NumberV(1), StringV("2"), DoubleV(3.4)), "bool", BooleanV(true), "num", NumberV(1234), "string", StringV("sup"), "float", DoubleV(1.234))))));
     Instance resp2 = resp2F.get().asInstance();
 
     assertTrue(resp.data().containsKey("testField"));
@@ -126,19 +126,19 @@ public class ClientSpec {
   public void testIssueQuery() throws IOException, ExecutionException, InterruptedException {
     String randomText1 = RandomStringUtils.randomAlphanumeric(8);
     String randomText2 = RandomStringUtils.randomAlphanumeric(8);
-    Ref classRef = Ref.create("classes/spells");
-    ListenableFuture<ResponseNode> createF1 = client.query(Create.create(classRef, ObjectV("data", ObjectV("queryTest1", StringV(randomText1)))));
-    ListenableFuture<ResponseNode> createF2 = client.query(Create.create(classRef, ObjectV("data", ObjectV("queryTest1", StringV(randomText2)))));
+    Ref classRef = Ref("classes/spells");
+    ListenableFuture<ResponseNode> createF1 = client.query(Create(classRef, ObjectV("data", ObjectV("queryTest1", StringV(randomText1)))));
+    ListenableFuture<ResponseNode> createF2 = client.query(Create(classRef, ObjectV("data", ObjectV("queryTest1", StringV(randomText2)))));
 
     Instance create1 = createF1.get().asInstance();
     Instance create2 = createF2.get().asInstance();
 
-    ListenableFuture<ResponseNode> queryF1 = client.query(Paginate.create(Match.create(StringV(randomText1), Ref.create("indexes/spells_by_test"))));
+    ListenableFuture<ResponseNode> queryF1 = client.query(Paginate(Match(StringV(randomText1), Ref("indexes/spells_by_test"))));
     Page page1 = queryF1.get().asPage();
     assertThat(page1.data().size(), is(1));
     assertThat(page1.data().get(0).asRef(), is(create1.ref()));
 
-    ListenableFuture<ResponseNode> queryF2 = client.query(Paginate.create(Match.create(classRef, Ref.create("indexes/spells_instances"))));
+    ListenableFuture<ResponseNode> queryF2 = client.query(Paginate(Match.create(classRef, Ref("indexes/spells_instances"))));
     Page page = queryF2.get().asPage();
 
     ImmutableList.Builder<Ref> refsBuilder = ImmutableList.builder();
@@ -157,10 +157,10 @@ public class ClientSpec {
     String randomText1 = RandomStringUtils.randomAlphanumeric(8);
     String randomText2 = RandomStringUtils.randomAlphanumeric(8);
 
-    Ref classRef = Ref.create("classes/spells");
+    Ref classRef = Ref("classes/spells");
 
-    Create expr1 = Create.create(classRef, ObjectV("data", ObjectV("queryTest1", StringV(randomText1))));
-    Create expr2 = Create.create(classRef, ObjectV("data", ObjectV("queryTest1", StringV(randomText2))));
+    Create expr1 = Create(classRef, ObjectV("data", ObjectV("queryTest1", StringV(randomText1))));
+    Create expr2 = Create(classRef, ObjectV("data", ObjectV("queryTest1", StringV(randomText2))));
 
     ListenableFuture<ImmutableList<ResponseNode>> createFuture = client.query(ImmutableList.of(expr1, expr2));
     ImmutableList<ResponseNode> results = createFuture.get();
@@ -176,24 +176,24 @@ public class ClientSpec {
     String randomText2 = RandomStringUtils.randomAlphanumeric(8);
     String randomText3 = RandomStringUtils.randomAlphanumeric(8);
 
-    Ref classRef = Ref.create("classes/spells");
+    Ref classRef = Ref("classes/spells");
 
-    ListenableFuture<ResponseNode> createFuture1 = client.query(Create.create(classRef, ObjectV("data", ObjectV("queryTest1", StringV(randomText1)))));
-    ListenableFuture<ResponseNode> createFuture2 = client.query(Create.create(classRef, ObjectV("data", ObjectV("queryTest1", StringV(randomText2)))));
-    ListenableFuture<ResponseNode> createFuture3 = client.query(Create.create(classRef, ObjectV("data", ObjectV("queryTest1", StringV(randomText3)))));
+    ListenableFuture<ResponseNode> createFuture1 = client.query(Create(classRef, ObjectV("data", ObjectV("queryTest1", StringV(randomText1)))));
+    ListenableFuture<ResponseNode> createFuture2 = client.query(Create(classRef, ObjectV("data", ObjectV("queryTest1", StringV(randomText2)))));
+    ListenableFuture<ResponseNode> createFuture3 = client.query(Create(classRef, ObjectV("data", ObjectV("queryTest1", StringV(randomText3)))));
 
     createFuture1.get();
     createFuture2.get();
     createFuture3.get();
 
-    ListenableFuture<ResponseNode> queryF = client.query(Paginate.create(Match.create(classRef, Ref.create("indexes/spells_instances"))).withSize(1));
+    ListenableFuture<ResponseNode> queryF = client.query(Paginate(Match(classRef, Ref("indexes/spells_instances"))).withSize(1));
     Page resp1 = queryF.get().asPage();
 
     assertThat(resp1.data().size(), is(1));
     assertThat(resp1.before(), not(Optional.<Ref>absent()));
     assertThat(resp1.after(), is(Optional.<Ref>absent()));
 
-    ListenableFuture<ResponseNode> queryF2 = client.query(Paginate.create(Match.create(classRef, Ref.create("indexes/spells_instances"))).withSize(1).withCursor(Cursor.Before.create(resp1.before().get())));
+    ListenableFuture<ResponseNode> queryF2 = client.query(Paginate(Match(classRef, Ref("indexes/spells_instances"))).withSize(1).withCursor(Before(resp1.before().get())));
     Page resp2 = queryF2.get().asPage();
 
     assertThat(resp2.data().size(), is(1));
@@ -207,15 +207,15 @@ public class ClientSpec {
     String randomText1 = RandomStringUtils.randomAlphanumeric(8);
     String randomText2 = RandomStringUtils.randomAlphanumeric(8);
 
-    Ref classRef = Ref.create("classes/spells");
+    Ref classRef = Ref("classes/spells");
 
-    ListenableFuture<ResponseNode> createFuture1 = client.query(Create.create(classRef, ObjectV("data", ObjectV("queryTest1", StringV(randomText1)))));
-    ListenableFuture<ResponseNode> createFuture2 = client.query(Create.create(classRef, ObjectV("data", ObjectV("queryTest1", StringV(randomText2)))));
+    ListenableFuture<ResponseNode> createFuture1 = client.query(Create(classRef, ObjectV("data", ObjectV("queryTest1", StringV(randomText1)))));
+    ListenableFuture<ResponseNode> createFuture2 = client.query(Create(classRef, ObjectV("data", ObjectV("queryTest1", StringV(randomText2)))));
 
     createFuture1.get();
     createFuture2.get();
 
-    ListenableFuture<ResponseNode> queryF = client.query(com.faunadb.client.java.query.Map.create(Lambda.create("x", Get.create(Var.create("x"))), Paginate.create(Match.create(Ref.create("classes/spells"), Ref.create("indexes/spells_instances"))).withSize(2)));
+    ListenableFuture<ResponseNode> queryF = client.query(Map(Lambda("x", Get(Var("x"))), Paginate(Match(Ref("classes/spells"), Ref("indexes/spells_instances"))).withSize(2)));
     Page resp = queryF.get().asPage();
 
     assertThat(resp.data().size(), is(2));
@@ -226,11 +226,11 @@ public class ClientSpec {
   @Test
   public void testHandleConstraintViolation() throws Throwable {
     String randomText = RandomStringUtils.randomAlphanumeric(8);
-    Ref classRef = Ref.create("classes/spells");
-    ListenableFuture<ResponseNode> createF = client.query(Create.create(classRef, ObjectV("data", ObjectV("uniqueTest1", StringV(randomText)))));
+    Ref classRef = Ref("classes/spells");
+    ListenableFuture<ResponseNode> createF = client.query(Create(classRef, ObjectV("data", ObjectV("uniqueTest1", StringV(randomText)))));
     createF.get();
 
-    ListenableFuture<ResponseNode> createF2 = client.query(Create.create(classRef, ObjectV("data", ObjectV("uniqueTest1", StringV(randomText)))));
+    ListenableFuture<ResponseNode> createF2 = client.query(Create(classRef, ObjectV("data", ObjectV("uniqueTest1", StringV(randomText)))));
     try {
       createF2.get();
     } catch (ExecutionException ex) {
