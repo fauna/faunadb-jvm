f6e03e9065a076d3705843eb8908d47bbdc080a4
Ed Ceaser <ed@faunadb.com>
2015-07-02 13:37:54 -0700

    scaladocs

--- faunadb-java/src/main/java/com/faunadb/client/java/errors/UnknownQueryException.java
+++ faunadb-java/src/main/java/com/faunadb/client/java/errors/UnknownQueryException.java
@@ -3,7 +3,7 @@ package com.faunadb.client.java.errors;
 import com.faunadb.client.java.HttpResponses;
 
 /**
- * An exception thrown if a query error is unknown or unparseable by the client.
+ * An exception thrown if a query error is unknown, or if it is unparseable.
  */
 public class UnknownQueryException extends QueryException {
   public UnknownQueryException(HttpResponses.QueryErrorResponse response) {
