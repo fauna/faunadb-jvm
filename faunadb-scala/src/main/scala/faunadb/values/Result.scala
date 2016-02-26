afa2ee37e23dee320dda18c842bc6b501afc7400
Matt Freels <matt@freels.name>
2016-02-26 14:33:32 -0800

    define isSuccess first, and others in terms of that

--- faunadb-scala/src/main/scala/faunadb/values/Result.scala
+++ faunadb-scala/src/main/scala/faunadb/values/Result.scala
@@ -39,14 +39,14 @@ object Result {
 sealed abstract class Result[+T] {
 
   def get: T
-  def isDefined: Boolean
+  def isSuccess: Boolean
 
   def map[U](f: T => U): Result[U]
   def flatMap[U](f: T => Result[U]): Result[U]
 
-  def isEmpty = !isDefined
-  def isSuccess = isDefined
-  def isFailure = !isDefined
+  def isFailure = !isSuccess
+  def isDefined = isSuccess
+  def isEmpty = isFailure
 
   def getOrElse[T1 >: T](alt: => T1) = try get catch { case ValueReadException(_) => alt }
   def toEither = try Right(get) catch { case ValueReadException(errs) => Left(errs) }
@@ -55,14 +55,14 @@ sealed abstract class Result[+T] {
 
 final case class VFail private[values] (errors: List[FieldError]) extends Result[Nothing] {
   def get = throw ValueReadException(errors)
-  def isDefined = false
+  def isSuccess = false
 
   def map[U](f: Nothing => U): Result[U] = VFail(errors)
   def flatMap[U](f: Nothing => Result[U]): Result[U] = VFail(errors)
 }
 
 final case class VSuccess[+T] private[values] (get: T, path: FieldPath) extends Result[T] {
-  def isDefined = true
+  def isSuccess = true
 
   def map[U](f: T => U): Result[U] = VSuccess(f(get), path)
   def flatMap[U](f: T => Result[U]): Result[U] = f(get)
