51bf3ef15d4957083d541c195909d6db647ce661
Erick Pintor <erickpintor@gmail.com>
2016-06-07 17:52:05 -0300

    documenting Field API

--- faunadb-java/src/main/java/com/faunadb/client/types/Field.java
+++ faunadb-java/src/main/java/com/faunadb/client/types/Field.java
@@ -1,15 +1,41 @@
 package com.faunadb.client.types;
 
+/**
+ * A field extractor for a FaunaDB {@link Value}
+ *
+ * @see Value
+ * @see Codec
+ */
 public final class Field<T> {
 
+  /**
+   * Creates a field that coerces its value using the codec passaed
+   *
+   * @param codec codec used to coerce the field's value
+   * @return the field extractor
+   */
   public static <T> Field<T> to(Codec<T> codec) {
     return new Field<>(Path.empty(), codec);
   }
 
+  /**
+   * Creates a field that extracts its value from a object path, assuming the value
+   * is an intance of {@link com.faunadb.client.types.Value.ObjectV}.
+   *
+   * @param keys path to the field
+   * @return the field extractor
+   */
   public static Field<Value> at(String... keys) {
     return new Field<>(Path.from(keys), Codec.VALUE);
   }
 
+  /**
+   * Creates a field that extracts its value from a array index, assuming the value
+   * is an intance of {@link com.faunadb.client.types.Value.ArrayV}.
+   *
+   * @param indexes path to the field
+   * @return the field extractor
+   */
   public static Field<Value> at(int... indexes) {
     return new Field<>(Path.from(indexes), Codec.VALUE);
   }
@@ -22,10 +48,22 @@ public final class Field<T> {
     this.codec = codec;
   }
 
+  /**
+   * Creates a field extractor composed with another nested field
+   *
+   * @param other nested field to compose with
+   * @return a new field extractor with the nested field
+   */
   public <A> Field<A> at(Field<A> other) {
     return new Field<>(path.subPath(other.path), other.codec);
   }
 
+  /**
+   * Creates a field extractor that coerce's its value using the codec passed
+   *
+   * @param codec codec to be used to coerce the field's value
+   * @return a new field that coerces its value using the codec passed
+   */
   public <A> Field<A> as(Codec<A> codec) {
     return new Field<>(path, codec);
   }
