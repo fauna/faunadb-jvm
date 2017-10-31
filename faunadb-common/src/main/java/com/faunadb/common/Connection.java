fba54efa2863d0144e66a09251c2f2208e152923
Ed Ceaser <ed@fauna.com>
2017-10-30 18:22:18 -0700

    Add support for txn time header

--- faunadb-common/src/main/java/com/faunadb/common/Connection.java
+++ faunadb-common/src/main/java/com/faunadb/common/Connection.java
@@ -21,6 +21,7 @@ import java.net.URL;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
 
 import static java.lang.String.format;
 
@@ -172,6 +173,7 @@ public final class Connection implements AutoCloseable {
   private final Logger log = LoggerFactory.getLogger(getClass());
   private final ObjectMapper json = new ObjectMapper();
   private final AtomicBoolean closed = new AtomicBoolean(false);
+  private final AtomicLong txnTime = new AtomicLong(0L);
 
   private Connection(URL faunaRoot, String authToken, RefAwareHttpClient client, MetricRegistry registry) {
     this.faunaRoot = faunaRoot;
@@ -294,10 +296,16 @@ public final class Connection implements AutoCloseable {
     final Timer.Context ctx = registry.timer("fauna-request").time();
     final SettableFuture<Response> rv = SettableFuture.create();
 
-    client.prepareRequest(request)
+    AsyncHttpClient.BoundRequestBuilder req = client.prepareRequest(request)
       .addHeader("Authorization", authHeader)
-      .setHeader("X-FaunaDB-API-Version", "2.1")
-      .execute(new AsyncCompletionHandler<Response>() {
+      .setHeader("X-FaunaDB-API-Version", "2.1");
+
+    long time = txnTime.get();
+    if (time > 0) {
+      req = req.setHeader("X-Last-Seen-Txn", Long.toString(time));
+    }
+
+    req.execute(new AsyncCompletionHandler<Response>() {
         @Override
         public void onThrowable(Throwable t) {
           ctx.stop();
@@ -309,6 +317,23 @@ public final class Connection implements AutoCloseable {
         public Response onCompleted(Response response) throws Exception {
           ctx.stop();
           rv.set(response);
+
+          String txnTimeHeader = response.getHeader("X-Txn-Time");
+          if (txnTimeHeader != null) {
+            long newTxnTime = Long.valueOf(txnTimeHeader);
+            boolean cas;
+            do {
+              long oldTxnTime = txnTime.get();
+
+              if (oldTxnTime < newTxnTime) {
+                cas = txnTime.compareAndSet(oldTxnTime, newTxnTime);
+              } else {
+                // Another query advanced the txnTime past this one.
+                break;
+              }
+            } while(!cas);
+          }
+
           logSuccess(request, response);
           return response;
         }
