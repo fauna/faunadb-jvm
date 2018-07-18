ef4a31b3b0cc8244518d6899ed347ef97aee5694
Brandon Mitchell <brandon@thewholedoubt.com>
2018-07-18 10:23:36 -0700

    Remove use of Defaults.defaultValue.

--- faunadb-java-dsl/src/main/java/com/faunadb/client/types/Decoder.java
+++ faunadb-java-dsl/src/main/java/com/faunadb/client/types/Decoder.java
@@ -11,7 +11,6 @@ import java.lang.reflect.Array;
 import java.lang.reflect.Type;
 import java.util.*;
 
-import static com.google.common.base.Defaults.defaultValue;
 import static java.lang.String.format;
 import static java.lang.reflect.Modifier.isAbstract;
 
@@ -32,6 +31,9 @@ import static java.lang.reflect.Modifier.isAbstract;
  */
 @SuppressWarnings("unchecked")
 public final class Decoder {
+  private static final Double DOUBLE_DEFAULT = Double.valueOf(0d);
+  private static final Float FLOAT_DEFAULT = Float.valueOf(0f);
+
   private Decoder() {
   }
 
@@ -117,6 +119,28 @@ public final class Decoder {
       return toMap(value, (MapType) dstType);
 
     return Codecs.getDecoder(rawType).decode(value).get();
+  }
+
+  private static <T> T defaultValue(Class<T> type) {
+    if (type == boolean.class) {
+      return (T) Boolean.FALSE;
+    } else if (type == char.class) {
+      return (T) Character.valueOf('\0');
+    } else if (type == byte.class) {
+      return (T) Byte.valueOf((byte) 0);
+    } else if (type == short.class) {
+      return (T) Short.valueOf((short) 0);
+    } else if (type == int.class) {
+      return (T) Integer.valueOf(0);
+    } else if (type == long.class) {
+      return (T) Long.valueOf(0L);
+    } else if (type == float.class) {
+      return (T) FLOAT_DEFAULT;
+    } else if (type == double.class) {
+      return (T) DOUBLE_DEFAULT;
+    } else {
+      return null;
+    }
   }
 
   private static Enum toEnum(Value value, Class<?> enumClass) {
