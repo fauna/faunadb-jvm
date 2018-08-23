bc904a9b856fb1c6874f1db55f4530cd7737e79b
Matt Freels <matt@freels.name>
2018-08-23 10:54:51 -0700

    drop c. prefix, rename `tag` to `tpe`, helper for module prefix, etc

--- faunadb-scala/src/main/scala/faunadb/values/CodecMacro.scala
+++ faunadb-scala/src/main/scala/faunadb/values/CodecMacro.scala
@@ -5,66 +5,69 @@ import scala.reflect.macros.blackbox
 class CodecMacro(val c: blackbox.Context) {
   import c.universe._
 
-  def caseClassImpl[T: c.WeakTypeTag]: c.Tree = {
-    val tag = weakTypeOf[T]
+  private val M = q"_root_.faunadb.values"
 
-    q""" new _root_.faunadb.values.Codec[$tag] {
-      override def decode(value: _root_.faunadb.values.Value, path: _root_.faunadb.values.FieldPath): _root_.faunadb.values.Result[$tag] =
-        ${getDecodedObject(tag)}
+  def caseClassImpl[T: WeakTypeTag]: Tree = {
+    val tpe = weakTypeOf[T]
 
-      override def encode(value: $tag): _root_.faunadb.values.Value =
-        ${getEncodedObject(tag)}
-    } """
+    q"""new $M.Codec[$tpe] {
+      def decode(value: $M.Value, path: $M.FieldPath): $M.Result[$tpe] =
+        ${getDecodedObject(tpe)}
+
+      def encode(value: $tpe): $M.Value =
+        ${getEncodedObject(tpe)}
+    }: $M.Codec[$tpe]"""
   }
 
-  def getEncodedObject(tag: c.Type): c.Tree = {
-    val fields = getFields(tag).map { field =>
+  private def getEncodedObject(tpe: Type): Tree = {
+    val fields = getFields(tpe) map { field =>
       val variable = q"value.${varName(field).toTermName}"
 
-      if (isOption(field._2))
-        q""" ${varName(field).toString} -> ($variable.map(v => v: _root_.faunadb.values.Value).getOrElse(_root_.faunadb.values.NullV)) """
-      else
-        q""" ${varName(field).toString} -> ($variable: _root_.faunadb.values.Value) """
+      if (isOption(field._2)) {
+        q"(${varName(field).toString}, ($variable.map(v => v: $M.Value).getOrElse($M.NullV)))"
+      } else {
+        q"(${varName(field).toString}, ($variable: $M.Value))"
+      }
     }
 
-    q"_root_.faunadb.values.ObjectV.apply(..$fields)"
+    q"$M.ObjectV(..$fields)"
   }
 
-  def getDecodedObject(tag: c.Type): c.Tree = {
-    val fields = getFields(tag)
+  private def getDecodedObject(tpe: Type): Tree = {
+    val fields = getFields(tpe)
 
-    val fieldsFragments = fields.map { field =>
+    val fieldsFragments = fields map { field =>
       val variable = q"value(${varName(field).toString})"
 
-      if (isOption(field._2))
-        fq"${varName(field)} <- _root_.faunadb.values.VSuccess($variable.to[${field._2.typeArgs.head}].toOpt, ${varName(field).toString})"
-      else
+      if (isOption(field._2)) {
+        fq"${varName(field)} <- $M.VSuccess($variable.to[${field._2.typeArgs.head}].toOpt, ${varName(field).toString})"
+      } else {
         fq"${varName(field)} <- $variable.to[${field._2}]"
+      }
     }
 
-    q"for (..$fieldsFragments) yield new $tag(..${fields.map(varName)})"
+    q"for (..$fieldsFragments) yield new $tpe(..${fields.map(varName)})"
   }
 
-  def isOption(tpe: c.Type) =
+  private def isOption(tpe: Type) =
     tpe.typeConstructor =:= weakTypeOf[Option[_]].typeConstructor
 
-  def varName(field: (c.Symbol, c.Type)): c.Name =
+  private def varName(field: (Symbol, Type)): Name =
     field._1.name.decodedName
 
-  def getFields(tag: c.Type): List[(c.Symbol, c.Type)] = {
-    if (tag.typeSymbol.isClass && !tag.typeSymbol.asClass.isCaseClass) {
-      throw new IllegalArgumentException(s"type $tag is not a case class")
+  private def getFields(tpe: Type): List[(Symbol, Type)] = {
+    if (tpe.typeSymbol.isClass && !tpe.typeSymbol.asClass.isCaseClass) {
+      throw new IllegalArgumentException(s"type $tpe is not a case class")
     }
 
-    val params = tag.decl(termNames.CONSTRUCTOR).asMethod.paramLists.flatten
-    val genericType = tag.typeConstructor.typeParams.map { _.asType.toType }
+    val params = tpe.decl(termNames.CONSTRUCTOR).asMethod.paramLists.flatten
+    val genericType = tpe.typeConstructor.typeParams.map { _.asType.toType }
 
     params.map { field =>
       val index = genericType.indexOf(field.typeSignature)
-      val fieldType = if (index >= 0) tag.typeArgs(index) else field.typeSignature
+      val fieldType = if (index >= 0) tpe.typeArgs(index) else field.typeSignature
 
       (field, fieldType)
     }
   }
 }
-
