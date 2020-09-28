8d9379c2d16344ab62ece5ddfa149ef749d8ac39
Leonardo Regnier <leonardoregnier@gmail.com>
2020-09-28 16:56:57 -0300

    DRV-264: add test case covering a 10 MB response

---
+++ faunadb-scala/src/load/scala/faunadb/FaunaClientLoadSpec.scala
@@ -0,0 +1,76 @@
+package faunadb
+
+import faunadb.query._
+import faunadb.values._
+import org.scalatest.{Matchers, fixture}
+import scala.concurrent.Future
+
+class FaunaClientLoadSpec extends fixture.AsyncWordSpec with Matchers with FaunaClientFixture {
+
+  "When submitting query whose response is about 10 MB of size, it" should {
+    "receive the response with no errors" in { client =>
+      // Fixtures
+      val collectionName = RandomGenerator.aRandomString
+
+      val documentFieldsNumber = 1000
+      val documentValuesSize = 1000
+      val documentsNumber = 10
+
+      val largeDocument: Expr = {
+        val fields: Seq[(String, Expr)] =
+          (1 to documentFieldsNumber).map { _ =>
+            val name = RandomGenerator.aRandomString
+            val value = Expr.encode(RandomGenerator.aRandomString(documentValuesSize))
+            (name -> value)
+          }
+
+        Obj("data" -> Obj(fields: _*))
+      }
+
+      def setup(): Future[(Value, Value)] = {
+        def createCollection(): Future[Value] = client.query(CreateCollection(Obj("name" -> collectionName)))
+
+        def createDocuments(): Future[Seq[Value]] = {
+          def createDocument(): Future[Value] = client.query(Do(Create(Collection(collectionName), largeDocument), Null))
+
+          Future.sequence(
+            (1 to documentsNumber).map { _ =>
+              createDocument()
+            }
+          )
+        }
+
+        val result: Future[(Value, Value)] =
+          for {
+            createCollectionResult <- createCollection()
+            createDocumentsResult <- createDocuments()
+          } yield (createCollectionResult, createDocumentsResult)
+
+        result
+      }
+
+      def run(): Future[Value] = {
+        // Get all documents
+        client.query {
+          Map(
+            Paginate(Documents(Collection(collectionName)), size = documentsNumber),
+            Lambda(ref => Get(ref))
+          )
+        }
+      }
+
+      // Run
+      val result: Future[Value] =
+        for {
+          _ <- setup()
+          result <- run()
+        } yield result
+
+      // Verify
+      result.map(_ => succeed) // If result is returned with no errors, succeed
+    }
+  }
+
+}
+
+
