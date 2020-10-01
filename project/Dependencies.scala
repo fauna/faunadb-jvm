b73833e2c45ddcecba19c5751179813319077327
Arnaud Gourlay <arnaud.gourlay@fauna.com>
2020-10-01 14:08:33 +0200

    harmcrest 2.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
     val scalaReflectScala212Version  = "2.12.12"
     val snakeYamlVersion             = "1.24"
     val junitInterfaceVersion        = "0.11"
-    val harmcrestLibraryVersion      = "2.1"
+    val harmcrestLibraryVersion      = "2.2"
     val junitVersion                 = "4.13"
   }
 
