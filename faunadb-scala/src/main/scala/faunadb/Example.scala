0fe7792d5cc9b0e39587f698acb89edf9d5391dc
Leonardo Regnier <leonardoregnier@gmail.com>
2020-11-09 16:31:57 -0300

    Add Example.scala

---
+++ faunadb-scala/src/main/scala/faunadb/Example.scala
@@ -0,0 +1,18 @@
+package faunadb
+
+import cats.syntax.option._
+
+class Account { /* ... */ }
+
+trait AccountService {
+  def getAccountById(id: Int): Option[Account]
+}
+
+class DummyAccountServiceImpl extends AccountService {
+
+  def dummyAccount: Account = ???
+
+  /* ... */
+  override def getAccountById(id: Int): Option[Account] = dummyAccount.some
+  /* ... */
+}
