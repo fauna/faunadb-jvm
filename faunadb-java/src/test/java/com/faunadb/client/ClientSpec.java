2f95c3c40ad9577d4195bf29ace03b7dc515e795
Vadim Balaban <vadim.balaban@fauna.com>
2021-09-13 10:19:16 +0300

    Updated test and pipeline

--- faunadb-java/src/test/java/com/faunadb/client/ClientSpec.java
+++ faunadb-java/src/test/java/com/faunadb/client/ClientSpec.java
@@ -1805,7 +1805,7 @@ public class ClientSpec {
   @Test
   public void shouldEvalDayOfWeekExpression() throws Exception {
     Value res = query(DayOfWeek(nowStr)).get();
-    long expected = cal.get(Calendar.DAY_OF_MONTH) - 1;
+    long expected = cal.get(Calendar.DAY_OF_WEEK) - 1;
     assertThat(res.to(LONG).get(), equalTo(expected));
   }
 
