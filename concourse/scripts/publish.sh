69b13f4373d9a0301b96663c5258eb4b38917861
Stanislav Parkhomenko <stanislav.parkhomenko@gmail.com>
2021-09-15 19:26:07 +0300

    chore: publishing script fix

--- concourse/scripts/publish.sh
+++ concourse/scripts/publish.sh
@@ -4,11 +4,17 @@ set -eou
 
 cd ./fauna-jvm-repository
 
+export SBT_OPTS="-Dsbt.log.noformat=true -Djava.net.preferIPv4Stack=true -Dhttp.connection.timeout=2 -Dhttp.connection-manager.timeout=2 -Dhttp.socket.timeout=6 -XX:+UseParallelGC -XX:ReservedCodeCacheSize=1G -Xss200M -Xms256M -Xmx6G"
+
 PACKAGE_VERSION=$(sbt faunadb-common/version | tail -n 1 | cut -c 8-)
 
 echo "Going to publish version: $PACKAGE_VERSION"
 
-export SBT_OPTS="-XX:+UseParallelGC -XX:ReservedCodeCacheSize=1G -Xss200M -Xms256M -Xmx6G -Dsbt.log.noformat=true -Djava.net.preferIPv4Stack=true -Dhttp.connection.timeout=2 -Dhttp.connection-manager.timeout=2 -Dhttp.socket.timeout=6"
+echo "$GPG_PRIVATE_KEY" > gpg_private_key
+echo "$GPG_PUBLIC_KEY" > gpg_public_key
+
+export GPG_PRIVATE_KEY=gpg_private_key
+export GPG_PUBLIC_KEY=gpg_public_key
 
 sbt faunadb-common/publishSigned
 sbt faunadb-java/publishSigned
