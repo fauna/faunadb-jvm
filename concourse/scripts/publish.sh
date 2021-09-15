702f72c6ac91b1da480d7f105f439573b60c5293
Stanislav Parkhomenko <stanislav.parkhomenko@gmail.com>
2021-09-15 14:46:50 +0300

    fix(OSS-862): Added sbt_opts settings (#320)

--- concourse/scripts/publish.sh
+++ concourse/scripts/publish.sh
@@ -8,6 +8,8 @@ PACKAGE_VERSION=$(sbt faunadb-common/version | tail -n 1 | cut -c 8-)
 
 echo "Going to publish version: $PACKAGE_VERSION"
 
+export SBT_OPTS="-XX:+UseParallelGC -XX:ReservedCodeCacheSize=1G -Xss200M -Xms256M -Xmx6G -Dsbt.log.noformat=true -Djava.net.preferIPv4Stack=true -Dhttp.connection.timeout=2 -Dhttp.connection-manager.timeout=2 -Dhttp.socket.timeout=6"
+
 sbt faunadb-common/publishSigned
 sbt faunadb-java/publishSigned
 sbt +faunadb-scala/publishSigned
