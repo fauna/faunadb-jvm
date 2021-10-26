741764124177a5f4e1e0957ca5a4d971a938a695
Vadim Balaban <vadim.balaban@fauna.com>
2021-10-26 15:15:36 +0300

    Roolback

--- faunadb-java/src/test/java/com/faunadb/client/ClientSpec.java
+++ faunadb-java/src/test/java/com/faunadb/client/ClientSpec.java
@@ -116,13 +116,13 @@ public class ClientSpec {
 
   @AfterClass
   public static void testDataCleanup() throws Exception {
-    System.out.println("******************************Environment Vars*****************************");
-    Map<String, String> enviorntmentVars  = System.getenv();
-    enviorntmentVars.entrySet().forEach(System.out::println);
-    System.out.println("***********************************************************");
-    if (System.getenv("CIRCLE_PULL_REQUESTS") != null) {
+//    System.out.println("******************************Environment Vars*****************************");
+//    Map<String, String> enviorntmentVars  = System.getenv();
+//    enviorntmentVars.entrySet().forEach(System.out::println);
+//    System.out.println("***********************************************************");
+//    if (System.getenv("CIRCLE_PULL_REQUESTS") != null) {
       rootClient.query(Delete(DB_REF)).handle((v, ex) -> handleBadRequest(v, ex)).get();
-    } else {
+   /* } else {
       rootClient.query(KeyFromSecret(ROOT_TOKEN))
               .handle((v, ex) -> handleBadRequest(v, ex))
               .thenApply((Value rootKey) ->
@@ -144,7 +144,7 @@ public class ClientSpec {
                                               Lambda(Value("ref"), If(Exists(Var("ref")), Delete(Var("ref")), NULL)))
                               )
                       ).handle(ClientSpec::handleBadRequest)).get();
-    }
+    }*/
 
   }
 
