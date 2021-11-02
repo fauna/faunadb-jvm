4bd1298d17ce2b05028f1f737718b2972267f871
Vadim Balaban <vadim.balaban@fauna.com>
2021-11-02 15:29:59 +0200

    Bumped version to 5.0.0-beta

--- project/Settings.scala
+++ project/Settings.scala
@@ -6,7 +6,7 @@ import scoverage.ScoverageSbtPlugin.autoImport._
 
 object Settings {
 
-  lazy val driverVersion = "4.2.0"
+  lazy val driverVersion = "5.0.0-beta"
 
   lazy val scala211 = "2.11.12"
   lazy val scala212 = "2.12.14"
