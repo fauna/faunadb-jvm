47861806d521694e8c7560986166e2daa1f9578c
Cleve Stuart <cleve.stuart@fauna.com>
2022-02-09 13:03:06 -0800

    Revert "OSS-895 [java] Driver needs to detect and throw an error upon loss of networking (#323)"
    This reverts commit e7d38cf84afa84b05b9593d7882d0e41f4c25b0c.

--- faunadb-scala/src/main/scala/faunadb/streaming/BodyValueFlowProcessor.scala
+++ faunadb-scala/src/main/scala/faunadb/streaming/BodyValueFlowProcessor.scala
@@ -2,22 +2,18 @@ package faunadb.streaming
 
 import java.nio.ByteBuffer
 import java.util
-import java.util.concurrent.{CompletableFuture, Flow, SubmissionPublisher}
+import java.util.concurrent.{Flow, SubmissionPublisher}
+
 import com.fasterxml.jackson.databind.{JsonNode, ObjectMapper}
 import faunadb.QueryError
-import faunadb.errors.{StreamingException, UnavailableException, UnknownException}
+import faunadb.errors.{StreamingException, UnknownException}
 import faunadb.values.{StringV, VSuccess, Value}
 import org.slf4j.LoggerFactory
 
-import java.net.http.HttpResponse
 import scala.collection.JavaConverters.asScalaIteratorConverter
-import scala.compat.java8.FutureConverters.CompletionStageOps
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.{Await, Future}
-import scala.util.{Failure, Success, Try}
+import scala.util.Try
 
-private [faunadb] class BodyValueFlowProcessor(json: ObjectMapper, syncLastTxnTime: Long => Unit,
-                                               connectionWatcher:() => CompletableFuture[HttpResponse[Flow.Publisher[java.util.List[ByteBuffer]]]] ) extends SubmissionPublisher[Value] with Flow.Processor[util.List[ByteBuffer], Value] {
+private [faunadb] class BodyValueFlowProcessor(json: ObjectMapper, syncLastTxnTime: Long => Unit) extends SubmissionPublisher[Value] with Flow.Processor[util.List[ByteBuffer], Value] {
   private val log = LoggerFactory.getLogger(getClass)
   private var subscription: Flow.Subscription = _
   private var subscriber: Flow.Subscriber[_ >: Value] = _
@@ -25,8 +21,6 @@ private [faunadb] class BodyValueFlowProcessor(json: ObjectMapper, syncLastTxnTi
   // We do not request data from the publisher until we have one subscriber
   // to avoid discarding events before the subscriber had the chance to subscribe.
   override def subscribe(subscriber: Flow.Subscriber[_ >: Value]): Unit = {
-    connectionWatcherStart(this.connectionWatcher)
-
     if (this.subscriber == null) {
       this.subscriber = subscriber
       super.subscribe(subscriber)
@@ -89,24 +83,5 @@ private [faunadb] class BodyValueFlowProcessor(json: ObjectMapper, syncLastTxnTi
 
   private def requestOne(): Unit =
     subscription.request(1)
-
-  private def connectionWatcherStart(connectionWatcher:() => CompletableFuture[HttpResponse[Flow.Publisher[java.util.List[ByteBuffer]]]]): Unit = {
-    val infiniteLoop: Future[Unit] = Future {
-      while (!Thread.currentThread().isInterrupted()) {
-        connectionWatcher()
-          .toScala
-          .flatMap {
-            case successResponse if successResponse.statusCode() < 300 => Future { successResponse }
-            case errorResponse => Future {onError(new Throwable(s"Status code: $errorResponse.statusCode() Body: $errorResponse.body()"))}
-          }
-        Thread.sleep(10000)
-      }
-    }
-
-    infiniteLoop.onComplete {
-      case Success(value) => println("Succeed with: " + value)
-      case Failure(e) => onError(e)
-    }
-  }
 }
 
